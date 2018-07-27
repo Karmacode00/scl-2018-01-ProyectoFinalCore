a952e050098ac39fe98937d67ce68fa53395f51f
Keylin_D <keylin.05.01@gmail.com>
2018-07-27 12:03:12 -0400

    animacion

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,5 @@
-var myVar;
+// Función para colocar  el cargador en la pagina y pueda mostrar el contenido de la pagina cuando se completa la carga
+let myVar;
 
 function myFunction() {
   myVar = setTimeout(showPage, 3000);
@@ -7,3 +8,5 @@ function showPage() {
   document.getElementById("loader").style.display="none";
   document.getElementById("myDiv").style.display="block";
 }
+// fin de la función
+
